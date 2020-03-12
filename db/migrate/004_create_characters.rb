f46e55de156b207f4247403a1fde127c44bacf06
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-03-12 08:01:58 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_characters.rb
+++ db/migrate/004_create_characters.rb
@@ -2,7 +2,6 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   def change 
     create_table :characters do |t|
       t.string :name 
-      t.string :actor_id
       t.integer :show_id
     end 
   end 
