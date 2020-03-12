02d71659bca3bbe7bc4e57106995ae0d2dc29949
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-03-12 08:08:18 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_catchphrase_to_characters.rb
+++ db/migrate/007_add_catchphrase_to_characters.rb
@@ -1,4 +1,4 @@
-class AddCatchphrase < ActiveRecord::Migration[5.1]
+class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
   def change
     add_column :characters, :catchphrase, :String
   end
